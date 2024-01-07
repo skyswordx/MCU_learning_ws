0806ca0e243dad4a0dcecec3c7207ea605d73215
Yuanyue <1140527828@qq.com>
2024-01-07 11:49:48 +0800

    学习到了静态数码管，初步完成了代码

---
+++ c51_project/2_1_lED/main.c
@@ -0,0 +1,35 @@
+#include <REGX52.H>
+#include <INTRINS.H>
+
+void Delay500ms()		//@11.0592MHz
+{
+	
+	
+
+	unsigned char i, j, k;
+
+	_nop_();
+	i = 4;
+	j = 129;
+	k = 119;
+	do
+	{
+		do
+		{
+			while (--k);
+		} while (--j);
+	} while (--i);
+}
+
+
+
+
+void main(){
+	while(1){
+		P2 = 0x00;
+		Delay500ms();
+		P2 = 0xFF;
+		Delay500ms();
+	}
+
+}
---
+++ c51_project/Static Digital Tube/main.c
@@ -0,0 +1,95 @@
+#include <REGX52.H>
+
+unsigned char NixieTable[] = {
+    0x3f, // 0 
+    0x06, // 1 
+    0x5b, // 2
+    0x4f, // 3
+    0x66, // 4
+    0x6d, // 5
+    0x7d, // 6
+    0x07, // 7
+    0x7f, // 8
+    0x6f, // 9
+    0x77, // A
+    0x7c, // b
+    0x39, // C
+    0x5e, // d
+    0x79, // E
+    0x71, // F
+    0x00, // 空
+};
+
+void NixieTube(unsigned char Location, unsigned char Number=17)
+{
+    switch (Location)
+    {
+        case 1:
+            P2_4 = 1;
+            P2_3 = 1;
+            P2_2 = 1;
+        
+            break;
+        case 2:
+            P2_4 = 1;
+            P2_3 = 1;
+            P2_2 = 0;
+            
+            break;
+
+        case 3:
+            P2_4 = 1;
+            P2_3 = 0;
+            P2_2 = 1;
+            
+            break;
+
+        case 4:
+            P2_4 = 1;
+            P2_3 = 0;
+            P2_2 = 0;
+            
+            break;
+
+        case 5:
+            P2_4 = 0;
+            P2_3 = 1;
+            P2_2 = 1;
+            
+            break;
+
+        case 6:
+            P2_4 = 0;
+            P2_3 = 1;
+            P2_2 = 0;
+            
+            break;
+        
+        case 7:
+            P2_4 = 0;
+            P2_3 = 0;
+            P2_2 = 1;
+            
+            break;
+
+        case 8:
+            P2_4 = 0;
+            P2_3 = 0;
+            P2_2 = 0;
+            
+            break;
+    }
+    P0 =NixieTable[Number];
+}
+
+
+void main(void)
+{
+    NixieTube(1, 1);
+    
+    while (1)
+    {
+
+    }
+   
+}
